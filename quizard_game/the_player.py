73355b40a93ae90c84791e495ea991078f3ba53c
Jim Roton <jim.roton@live.com>
2024-03-08 15:49:54 -0600

    quizard game updated

--- quizard_game/the_player.py
+++ quizard_game/the_player.py
@@ -1,37 +1,58 @@
+import time
 import the_quizard as the_quizard
 
 class player:
 
     def start_game(self):
+        quotes = the_quizard.quizard_quotes
 
-        print("Welcome to the Quizard Game!!!")
-        print("I'm your host... The Quizard!!!")
-        print("Let's meet our contestant!!!")
+        print(quotes.get_quote(0))
+        time.sleep(2)
+        print(quotes.get_quote(1))
+        time.sleep(2)
+        print(quotes.get_quote(2))
+        time.sleep(2)
 
-        self.player_name = input("Tell us your name player: ")
+        self.player_name = input(quotes.get_question(0))
 
-        print(f"Welcome to the game {self.player_name}!!!")
-        print("Let's get started!!!")
+        print(f"{quotes.get_quote(3)}".replace("%player_name%", self.player_name))
+        time.sleep(4)
+        print(quotes.get_quote(4))
+        time.sleep(2)
 
-        self.question_level = input(f"{self.player_name}, first pick your question dificulty (1/2/3): ")
-        self.question_type = input(f"{self.player_name}, now pick your question type (1/2/3/4): ")
-        
-        print(f"The player chose {self.__question_level_to_name__(self.question_level)}")
+        # choose question type and level
+        self.question_type = int(input(f"{quotes.get_question(1)}".replace("%player_name%", self.player_name)))
+        time.sleep(1)
+        print(f"{quotes.get_quote(5)}".replace("%question_type%", self.__question_type_to_name__(self.question_type)))
+        time.sleep(1)
+        self.question_level = int(input(quotes.get_question(2)))
+        time.sleep(1)
+        print(f"{quotes.get_quote(6)}".replace("%question_level%", self.__question_level_to_name__(self.question_level)))
+        time.sleep(3)
+        print(quotes.get_quote(7))
+        time.sleep(2)
 
-        print("That's great, here's your first question...")
-        # question_level = input("")
-
-    def __question_level_to_name__(self, question_level: int) -> str:
-        if (question_level == 1):
+    def __question_type_to_name__(self, question_type: int) -> str:
+        if (question_type == 1):
             return "Addition"
-        elif (question_level == 2):
+        elif (question_type == 2):
             return "Subtraction"
-        elif (question_level == 3):
+        elif (question_type == 3):
             return "Multiplication"
         else:
             return "Division"
 
-
+    def __question_level_to_name__(self, question_level: int) -> str:
+        if (question_level == 1):
+            return "Easy"
+        elif (question_level == 2):
+            return "Medium"
+        else:
+            return "Hard"
 
 p = player()
 p.start_game()
+q = the_quizard.quizard(p.question_type, p.question_level)
+
+question = q.get_question()
+print(question.get_question_statement())
--- quizard_game/the_quizard.py
+++ quizard_game/the_quizard.py
@@ -1,6 +1,69 @@
+class question:
+    def __init__(self, num1: int, num2: int, question_type: int):
+        self.num1 = num1
+        self.num2 = num2
+        self.question_type = question_type
+
+    def get_question_statement(self) -> str:
+        return f"{self.num1} {self.__question_type_to_symbol__(self.question_type)} {self.num2}"
+    
+    def __question_type_to_symbol__(self, question_type: int) -> str:
+        if (question_type == 1):
+            return "+"
+        elif (question_type == 2):
+            return "-"
+        elif (question_type == 3):
+            return "*"
+        else:
+            return "/"
+    
+    # def question_answer(self, num1: int, num2: int, question_type: int) -> int:
+    #     if (question_type == 1):
+    #         return num1 + num2
+    #     elif (question_type == 2):
+    #         return num1 - num2
+    #     elif (question_type == 3):
+    #         return num1 * num2
+    #     else:
+    #         return num1 / num2
+
+class quizard_quotes:
+    quotes = [
+        "Welcome to the Quizard Game!!!",
+        "I'm your host... The Quizard!!!",
+        "Let's meet our contestant!!!",
+        "Welcome to the game %player_name%!!!",
+        "Let's get started!!!",
+        "Great you chose %question_type%!",
+        "Great you chose %question_level%!",
+        "Let's get started with your first question."
+    ]
+
+    questions = [
+        "Tell us your name player:",
+        "%player_name%, first pick your question type (1/2/3/4):",
+        "And now pick your question level (1/2/3):"
+    ]
+
+    successes = []
+
+    failures = []
+
+    def get_quote(idx: int) -> str:
+        return quizard_quotes.quotes[idx]
+    
+    def get_question(idx: int) -> str:
+        return quizard_quotes.questions[idx]
+    
+    def get_random_success() -> str:
+        pass
+
+    def get_random_failure() -> str:
+        pass
+
 class quizard:
 
-    def __init__(self, question_type: int, question_level: int):
+    def __init__(self, question_type: int, question_level: int) -> question:
         self.question_type = question_type
         self.question_level = question_level
     
@@ -8,7 +71,8 @@ class quizard:
         
         #addition
         if (self.question_type == 1):
-            print("addition")
+            q = question(1, 5, 1)
+            return q
 
         #subtraction
         elif (self.question_type == 2):
